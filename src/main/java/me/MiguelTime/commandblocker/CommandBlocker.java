e7ed98628d2c6f428dfb2cfdfeab17ad088be8e2
MiguelTime <miguelpandares690@gmail.com>
2025-07-09 20:11:52 -0400

    Add configurable messages to plugin
    Replaced hardcoded plugin messages with configurable entries in config.yml. Added a getMessage method to CommandBlocker for retrieving and formatting messages. Updated CBCommand and CommandListener to use the new message system. Bumped plugin version to 1.1.

--- src/main/java/me/MiguelTime/commandblocker/CommandBlocker.java
+++ src/main/java/me/MiguelTime/commandblocker/CommandBlocker.java
@@ -1,6 +1,8 @@
 package me.MiguelTime.commandblocker;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.ChatColor;
+
 
 public class CommandBlocker extends JavaPlugin {
 
@@ -16,6 +18,11 @@ public class CommandBlocker extends JavaPlugin {
         getCommand("cbtest").setExecutor(new CBCommand());
         getLogger().info("CommandBlocker activado correctamente.");
     }
+    public String getMessage(String path) {
+        return ChatColor.translateAlternateColorCodes('&',
+                getConfig().getString("messages." + path, "§c[Mensaje no definido]"));
+    }
+
 
     public static CommandBlocker getInstance() {
         return instance;
